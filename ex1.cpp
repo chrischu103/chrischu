e1690f17c35582bfb95c1c9c3fbf56c2ef3c2a0d
chrischu103 <100347499+chrischu103@users.noreply.github.com>
2022-02-24 18:50:13 +0800

    Create ex1.cpp
    老師教printf and cout 的程式設計,然後寫成文章

---
+++ ex1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+
+{   printf("I am 15 years old. My weight is 65 kg. \n  ");
+    printf("I height is 185 cm");
+    printf("I have %d brother. %d brother is University. %d is junior high school.\n",2,1,1);
+    printf("My parents are also learn programming. \n");
+    return 0;
+}
